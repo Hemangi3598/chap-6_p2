2007d26e2c99659bf258c2d8425a6984e960639c
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 17:13:38 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,12 @@
+# wapf to accept as input two par and 
+# return their addition ka result
+
+def add(n1, n2):
+	res = n1 + n2
+	return res
+
+m1 = m1 = float(input(" enter first number "))
+m2 = float(input(" enter second number "))
+
+ans = add(m1, m2)
+print("ans = ", ans)
